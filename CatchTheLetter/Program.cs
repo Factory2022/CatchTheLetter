c695e8da6a613b08e06512d150d19306a01579c6
Christian Steiner <coder@der-tueftler.de>
2022-09-11 12:07:41 +0200

    Timer eingefügt - so soll eine feste Framr<te eingehalten werden.

--- CatchTheLetter/Program.cs
+++ CatchTheLetter/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace CatchTheLetter
 {
@@ -11,83 +13,128 @@ namespace CatchTheLetter
     {
         public static int direction, barSpeed , width, hight;
         public static int xPositionOld, xPosition;
-        static void Main(string[] args)
+        public static ConsoleKeyInfo cki;
+        public static int counter, marker1;
+        
+        
+        static void Main()
         {
             width = 100;
             hight = 45;
             xPosition = width / 2;
             xPositionOld = xPosition;
-            barSpeed = 4;
+            barSpeed = 3;
+            counter = 0;
+            
+            var timer = new Stopwatch();
+
+            Thread AskTheKey = new Thread(AskKey);
+            AskTheKey.Start();
+
+            Thread StopMoving = new Thread(DiectionOff);
+            StopMoving.Start();
 
 
             Console.WindowHeight = hight;
             Console.WindowWidth = width;
-
             Console.CursorLeft = xPosition;
-            Console.CursorTop = hight - 5;
+            Console.CursorTop = hight -5 ;
             Console.Write(@"\______/");
 
 
-            // Thread für die Tastatureingab erstellen
-            Thread KeyInmput = new Thread(AskAKey);
-            KeyInmput.Start();
+           // public static ConsoleKeyInfo cki;
 
             while (true)
             {
+
+                DrawPlayer();
                 Thread.Sleep(20);
                 
-                if (direction == 1)
-                {
-                    xPosition += barSpeed;
-                    if (xPosition > width-9) xPosition = width-9;
-                    
-                }
-                
-                
-                if (direction == -1)
-                {
-                    xPosition -= barSpeed;
-                    if (xPosition < 1) xPosition = 1;
-                }
-                
-                Console.CursorLeft = xPositionOld;
-                Console.CursorTop = hight - 5;
-                if (direction != 0) Console.Write("        ");
-                    
+                timer.Start();
+                //B: Run stuff you want timed
+                //timer.Stop();
 
-                Console.CursorLeft = xPosition;
-                // Console.CursorTop  = hight-5;
-                if (direction != 0) Console.Write(@"\______/");
+                TimeSpan timeTaken = timer.Elapsed;
+                // Console.WriteLine(timeTaken);
 
-                xPositionOld = xPosition;
-                Console.CursorLeft = 1;
-                Console.CursorTop = hight;
-
-                //direction = 0;
             }
 
-            Console.ReadKey();
+
 
         }
 
-        public static void AskAKey()           // Get Key Methode
+
+        public static void DrawPlayer()          
         {
-            ConsoleKeyInfo c = new ConsoleKeyInfo();
-
-            while(true)
+            
+            if (direction == 1)
             {
-                Thread.Sleep(20);          // Zeit verschwenden und Prozessorlast senken...
-               
-                
-                c = Console.ReadKey(true);
-                string test = Convert.ToString(c.Key);
-                if (test == "LeftArrow")    direction = -1;
-                if (test == "RightArrow")   direction = 1;
-               
-
+                xPosition += barSpeed;
+                if (xPosition > width - 9) xPosition = width - 9;
             }
 
 
+            if (direction == -1)
+            {
+                xPosition -= barSpeed;
+                if (xPosition < 1) xPosition = 1;
+            }
+
+            if (xPositionOld < 2) xPositionOld = 2;
+            Console.CursorLeft = xPositionOld-1;
+            Console.CursorTop = hight - 5;
+            //if (direction != 0) Console.Write("           ");
+            Console.Write("           ");
+
+
+            Console.CursorLeft = xPosition;
+            Console.CursorTop = hight - 5;
+            //if (direction != 0) Console.Write(@"\______/");
+            Console.Write(@"\______/");
+
+            xPositionOld = xPosition;
+            
+            Console.CursorLeft = 1;
+            Console.CursorTop = hight - 1;
+
+            
+
+        }
+
+        public static void AskKey()
+        {
+            
+            while (true)
+            {
+
+                
+
+                while (Console.KeyAvailable == true)
+                {
+                    cki = Console.ReadKey(true);
+
+                    if (cki.Key == ConsoleKey.LeftArrow) direction = -1;
+                    if (cki.Key == ConsoleKey.RightArrow)  direction = 1;
+                    if (cki.Key != ConsoleKey.RightArrow && cki.Key != ConsoleKey.LeftArrow) direction = 0;
+                    
+                }
+               Thread.Sleep(20);
+               
+                
+                //Control.KeyUp;
+
+
+
+            }
+        }
+
+        public static void DiectionOff()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                //if (Console.KeyAvailable == false) direction = 0;
+            }
         }
     }
 }
